d39a175442137c5978b6f7442adb21529cbdd148
Max <veyronmclord@gmail.com>
2021-01-15 16:47:20 +0600

    testing deploy with build version

---
+++ build/precache-manifest.cf5dc185e96d186c8a4ac6f4282172c0.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "260535e2d02008be0af0f02d67d69da6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04b77fb39f76a606668d",
+    "url": "/static/css/2.8bad4ec3.chunk.css"
+  },
+  {
+    "revision": "db1d6cae6f0c4ee077a0",
+    "url": "/static/css/main.b2f2c0d4.chunk.css"
+  },
+  {
+    "revision": "04b77fb39f76a606668d",
+    "url": "/static/js/2.fa85ed95.chunk.js"
+  },
+  {
+    "revision": "1600bbbcfd04f9d75c74396d34eb3b52",
+    "url": "/static/js/2.fa85ed95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db1d6cae6f0c4ee077a0",
+    "url": "/static/js/main.ea6454a8.chunk.js"
+  },
+  {
+    "revision": "f11091f1eb6fc110ce4c",
+    "url": "/static/js/runtime-main.77f7dd24.js"
+  },
+  {
+    "revision": "6a4dacaac6c73c8ade319c1b0680de39",
+    "url": "/static/media/Frame_67.6a4dacaa.png"
+  },
+  {
+    "revision": "759519507dbd5e5a6e798e3ab13ffefb",
+    "url": "/static/media/gps.75951950.svg"
+  },
+  {
+    "revision": "69a0b33a54b0e3e9e66feed33d721c12",
+    "url": "/static/media/image16.69a0b33a.png"
+  },
+  {
+    "revision": "d41c5a8d55442e4520f567f47ed466af",
+    "url": "/static/media/image_10.d41c5a8d.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
