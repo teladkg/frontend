7a671989706379734170ff9f436b3c6cb3161f4f
Max <veyronmclord@gmail.com>
2021-01-15 18:59:39 +0600

    updated server js file

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(__dirname, 'build'));
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
