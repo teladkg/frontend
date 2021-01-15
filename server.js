5056784116674e7bd9aeaa6bdc3afca133e95c17
Max <veyronmclord@gmail.com>
2021-01-15 20:33:45 +0600

    returning server js test code

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// app.use(express.static(path.join(__dirname, 'build')));
-app.use(express.static(__dirname, 'build'));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
